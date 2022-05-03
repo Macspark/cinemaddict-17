82b68113ef119fb76c3ab53563218d31109cecef
macspark <delwexx@gmail.com>
2022-05-03 17:49:07 +0300

    Сделал подгрузку фильмов по 5 штук

--- src/main.js
+++ src/main.js
@@ -4,12 +4,8 @@ import NavigationView from './view/navigation.js';
 import SortView from './view/sort.js';
 import StatisticsView from './view/statistics.js';
 import BoardPresenter from './presenter/board-presenter.js';
-
 import MoviesModel from './model/movies-model.js';
 import CommentModel from './model/comments-model.js';
-
-const movieModel = new MoviesModel();
-const commentModel = new CommentModel();
 
 const EntryPoints = {
   HEADER: document.querySelector('.header'),
@@ -17,12 +13,14 @@ const EntryPoints = {
   FOOTER: document.querySelector('.footer')
 };
 
-const boardPresenter = new BoardPresenter();
+const movieModel = new MoviesModel();
+const commentModel = new CommentModel();
+const boardPresenter = new BoardPresenter(EntryPoints, movieModel, commentModel);
 
 render(new UserView(), EntryPoints.HEADER);
 render(new NavigationView(), EntryPoints.MAIN);
 render(new SortView(), EntryPoints.MAIN);
 
-boardPresenter.init(EntryPoints, movieModel, commentModel);
+boardPresenter.init();
 
 render(new StatisticsView(), EntryPoints.FOOTER);
