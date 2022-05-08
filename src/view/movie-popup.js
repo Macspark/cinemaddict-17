63cd2e40b75ae71313402e0f8ed4c66dc5c21f5a
macspark <delwexx@gmail.com>
2022-05-08 21:46:19 +0300

    removed unnecessary condition

--- src/view/movie-popup.js
+++ src/view/movie-popup.js
@@ -129,7 +129,7 @@ const createMoviePopupTemplate = (movie, comments) => {
 
             <div class="film-details__bottom-container">
               <section class="film-details__comments-wrap">
-                <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments && comments.length ? comments.length : '0'}</span></h3>
+                <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
 
                 <ul class="film-details__comments-list">
                   ${createCommentsLayout(comments)}
